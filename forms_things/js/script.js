0a866bfd0d2e34f15e0971b1a9fe97a140da2708
Sara Holburt <sara.holburt@gmail.com>
2018-06-13 15:25:20 -1000

    Persisting form elements

--- forms_things/js/script.js
+++ forms_things/js/script.js
@@ -3,4 +3,29 @@ $(document).ready(function(){
 		$('#resume-upload-trigger').click(function() {
 			$("#resume-upload").click();
 		});
+
+		var applicants = [];
+		$('#submit-job').click(function(dontSend) {
+			dontSend.preventDefault();
+			var name = $("#fullname").val();
+			var address = $("#address").val();
+			var city = $("#city").val();
+			var state = $("#state").val();
+			var zipcode = $("#zipcode").val();
+			var phone = $("#phone").val();
+			var email = $("#email").val();
+
+			var applicant = {
+				name:name,
+				address:address,
+				city:city,
+				state:state,
+				zipcode:zipcode,
+				phone:phone,
+				email:email
+			}
+
+			applicants.push(applicant);
+			console.log(applicants);
+		});
 });
